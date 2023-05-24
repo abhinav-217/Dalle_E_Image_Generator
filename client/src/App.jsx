6b74b70487ece1d7c200ae8728b3b21c4b36a1a0
abhinav-217 <abhinavsaxena217@gmail.com>
2023-05-24 12:59:24 +0530

    Added the newLogo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
-import {logo} from './assets';
+import {logo,newLogo} from './assets';
 import {Home, CreatePost,SubmitPost} from './pages';
 import './App.css'
 const app = () => {
@@ -8,7 +8,7 @@ const app = () => {
       <BrowserRouter>
         <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to = '/'>
-          <img src = {logo} alt = 'logo' className = "w-28 object-contain "/>
+          <img src = {newLogo} alt = 'logo' className = "w-15 object-contain "/>
         </Link>
         <Link to = '/create-post' className = "font-inter font-medium bg-[#6409ff] text-white px-4 py-2 rounded-md">
           Create
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,10 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
-
+import newLogo from './newLogo.svg'
 export {
   download,
   logo,
   preview,
+  newLogo,
 };
